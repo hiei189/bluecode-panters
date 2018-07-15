cc4f2feed0c172efdac64a2c29129be11b6b2e97
José Chávez <jsergio.chavez@gmail.com>
2018-07-15 04:12:33 -0500

    port

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, (err, res) => {
-  console.log('LISTENING ON PORT ' + process.env.PORT);
+app.listen(process.env.VCAP_APP_PORT || 3000, (err, res) => {
+  console.log('LISTENING ON PORT ' + process.env.VCAP_APP_PORT);
 });
